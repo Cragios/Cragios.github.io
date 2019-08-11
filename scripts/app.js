3bc267629ba1fae9359d9006d18733891e48c32f
Cragios <cheehiantai@gmail.com>
2019-08-11 10:49:29 +0800

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -42,17 +42,17 @@
     ////////////////////////////////////////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    // if ('serviceWorker' in navigator) {
-    //     window.addEventListener('load', () => {
-    //         navigator.serviceWorker.register('service-worker.js')
-    //             .then(function (registration) {
-    //                 console.log('[App] Service Worker is registered', registration);
-    //             })
-    //             .catch(function (error) {
-    //                 console.error('[App] Service Worker registration failed', error);
-    //             });
-    //     });
-    // } else {
-    //     alert('Your browser does not support service workers!');
-    // }
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('service-worker.js')
+                .then(function (registration) {
+                    console.log('[App] Service Worker is registered', registration);
+                })
+                .catch(function (error) {
+                    console.error('[App] Service Worker registration failed', error);
+                });
+        });
+    } else {
+        alert('Your browser does not support service workers!');
+    }
 })();
